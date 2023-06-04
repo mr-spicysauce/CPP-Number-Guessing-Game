5162b11a38bce52108e46765e6b847cbd8f2bb68
mr_spicysauce <mr_spicysauce@yahoo.com>
2023-06-04 20:30:23 +0100

    👌 IMPROVE: Made the game loop

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+void playAgain();
+
 int main(){
 
     //this generates a random seed based on the time and the seed us used to make a random number between 1-100
@@ -15,8 +17,6 @@ int main(){
 
     cout <<"A random number from 1-100 has been picked" << endl;
     cout <<"You have 5 attempts to guess the number" << endl;
-
-    cout << numToGuess;
 
     while (attempts >= 1)
     {
@@ -31,14 +31,25 @@ int main(){
        }
        else{
         cout <<currentGuess << "is the correct number!" << endl;
-        cout << "you guessed the number in " << (4 - attempts) << " guesses!";
-        return 0;
+        cout << "you guessed the number in " << (4 - attempts) << " guesses!" << endl;
+        playAgain();
        }
 
        attempts--;
     }
 
-    cout <<"You could not get it in 5 attempts!"<<endl << "the correct number was: " << numToGuess;
+    cout <<"You could not get it in 5 attempts!"<<endl << "the correct number was: " << numToGuess << endl;
+
+    playAgain();
 
     return 0;
+}
+
+void playAgain(){
+    char yORn;
+    cout << "Want to play again? y/n" << endl;
+    cin >> yORn;
+    if (yORn == 'y'){
+        main();
+    }
 }
